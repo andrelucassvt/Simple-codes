82835927edc2462df13d63322b0dc7b2fd9c9323
André_Lucas <58487111+andrelucassvt@users.noreply.github.com>
2020-03-12 16:16:57 -0300

    Update Pessoas.java

--- Java-JFrame-NumberRandom/Aleatorio/Pessoas.java
+++ Java-JFrame-NumberRandom/Aleatorio/Pessoas.java
@@ -19,7 +19,27 @@ public class Pessoas {
         this.nome[1] = "Erick Lima";
         this.nome[2] = "Andrei Victor";
         this.nome[3] = "Tayna Silva";
-        this.nome[4] = 
+        this.nome[4] = "Raimundo Neto";
+        this.nome[5] = "Willdeberg Vianna";
+        this.nome[6] = "Dennis Levy";
+        this.nome[7] = "Gabriel melo";
+        this.nome[8] = "Gabriel da Silva";
+        this.nome[9] = "Felipe Fonseca";
+        this.nome[10] = "Leonardo Pereira";
+        this.nome[11] = "João Victor Fernandes";
+        this.nome[12] = "Victor Alencar";
+        this.nome[13] = "Lucas Saraiva";
+        this.nome[14] = "Carlos Lobato";
+        this.nome[15] = "Nicolas Correa";
+        this.nome[16] = "Hugo Teixeira";
+        this.nome[17] = "Hermann Klaus";
+        this.nome[18] = "Gabriel Henriques";
+        this.nome[19] = "Brunno de Oliveira";
+        this.nome[20] = "Raimmundo Marque";
+        this.nome[21] = "Aluizio Dias";
+        this.nome[22] = "Victor Carvalho";
+        this.nome[23] = "Victor Aguiar"; 
+            
         
         return this.nome[x];
         
