6d411a2c2a79946ad2ded815e32b1b3875ceee8e
andrelucassvt <andrelucassvt99@gmail.com>
2020-03-12 20:40:08 -0300

    Programando um elevador, exercicio da web

---
+++ Exercicio-Elevador/src/Elevador/Test.java
@@ -0,0 +1,18 @@
+
+package test;
+
+
+public class Test {
+
+  
+    public static void main(String[] args) {
+        
+        Elevador l1 = new Elevador(10,10);
+        l1.acrescentarPessoa();
+        l1.subir();
+        l1.acrescentarPessoa();
+        l1.subir();
+        
+    }
+    
+}
