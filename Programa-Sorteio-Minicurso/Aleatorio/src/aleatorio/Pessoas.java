180094b6c461ded29ecc5b71d56ed78c82b10c2a
ANDRE LUCAS BARBOSA SALVADOR <04041884@sempreunama.com.br>
2020-03-13 13:34:22 -0300

    Código para sorteio da caneca

---
+++ Programa-Sorteio-Minicurso/Aleatorio/src/aleatorio/Pessoas.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aleatorio;
+
+/**
+ *
+ * @author andre
+ */
+public class Pessoas {
+    
+    private String nome [] = new String[30];
+    
+    
+    public String lista(int x){
+        this.nome[0] = "Jorhan Chagas";
+        this.nome[1] = "Erick Lima";
+        this.nome[2] = "Andrei Victor";
+        this.nome[3] = "Tayna Silva";
+        this.nome[4] = "Raimundo Neto";
+        this.nome[5] = "Willdeberg Vianna";
+        this.nome[6] = "Dennis Levy";
+        this.nome[7] = "Gabriel melo";
+        this.nome[8] = "Gabriel da Silva";
+        this.nome[9] = "Felipe Fonseca";
+        this.nome[10] = "Leonardo Pereira";
+        this.nome[11] = "João Victor Fernandes";
+        this.nome[12] = "Victor Alencar";
+        this.nome[13] = "Lucas Saraiva";
+        this.nome[14] = "Carlos Lobato";
+        this.nome[15] = "Nicolas Correa";
+        this.nome[16] = "Hugo Teixeira";
+        this.nome[17] = "Hermann Klaus";
+        this.nome[18] = "Gabriel Henriques";
+        this.nome[19] = "Brunno de Oliveira";
+        this.nome[20] = "Raimmundo Marque";
+        this.nome[21] = "Aluizio Dias";
+        this.nome[22] = "Victor Carvalho";
+        this.nome[23] = "Victor Aguiar"; 
+        this.nome[24] = "João Gabriel";
+        this.nome[25] = "Ingrid Shizue";
+            
+        
+        return this.nome[x];
+        
+    }
+    
+    
+    
+ 
+    
+    
+    
+}
